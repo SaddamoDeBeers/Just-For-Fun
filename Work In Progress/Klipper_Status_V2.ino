22056751b3a240494c29b8161c8523597f1b5a31
Adam De Beers <30910405+SaddamoDeBeers@users.noreply.github.com>
2025-07-23 23:52:57 +0200

    Update Klipper_Status_V2.ino

--- Work In Progress/Klipper_Status_V2.ino
+++ Work In Progress/Klipper_Status_V2.ino
@@ -2,8 +2,8 @@
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
 
-const char* ssid = "VM4272188NN";
-const char* password = "sxjbYuekscx2";
+const char* ssid = "Your Wifi";
+const char* password = "Your Wifi Pass";
 
 const char* printerIP = "192.168.1.35";  // Replace with your Klipper IP
 const int printerPort = 7125;
